d6e1bcfdcec4c3d3618fe8b523addd4430627bbe
태라 <terra2007@naver.com>
2023-02-16 07:59:02 +0900

    Add model.py

---
+++ model.py
@@ -0,0 +1,71 @@
+"""
+file name: model.py
+
+create time: 2023-02-16 07:27
+author: Tera Ha
+e-mail: terra2007@naver.com
+github: https://github.com/terra2007
+"""
+import torch.nn as nn
+
+
+class AlexNet(nn.Module):
+    """
+    Neural network model proposed in the AlexNet paper.
+    """
+    def __init__(self, num_classes=1000):
+        """
+        Args:
+            num_classes (int) : number of classes the model will predict.
+        """
+        super(AlexNet, self).__init__()
+        # input size : (b x 3 x 227 x 227)
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),  # (b x 96 x 55 x 55)
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 96 x 27 x 27)
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, padding=2),  # (b x 256 x 27 x 27)
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 256 x 13 x 13)
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, padding=1),  # (b x 384 x 13 x 13)
+            nn.ReLU(),
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, padding=1),  # (b x 384 x 13 x 13)
+            nn.ReLU(),
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),  # (b x 256 x 13 x 13)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2)  # (b x 256 x 6 x 6)
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=(256 * 6 * 6), out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(),
+            nn.Linear(in_features=4096, out_features=num_classes)
+        )
+        self.init_bias()  # initialize bias
+
+    def init_bias(self):
+        for layer in self.net:
+            if isinstance(layer, nn.Conv2d):
+                nn.init.normal_(layer.weight, mean=0, std=0.01)
+                nn.init.constant_(layer.bias, 0)
+
+        nn.init.constant_(self.net[4].bias, 1)
+        nn.init.constant_(self.net[10].bias, 1)
+        nn.init.constant_(self.net[12].bias, 1)
+
+    def forward(self, x):
+        """
+        Args:
+            x (Tensor) : input tensor
+
+        Returns:
+            output (Tensor) : output tensor
+        """
+        x = self.net(x)
+        x = x.view(-1, 256 * 6 * 6)
+        return self.classifier(x)
